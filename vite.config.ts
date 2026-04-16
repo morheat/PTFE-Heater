2f00c234afdaade70a332d59f6f1cfbedd8450d2
morheat <mpicoopstudent@gmail.com>
2026-04-16 14:32:15 -0400

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import svgr from "vite-plugin-svgr";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/PTFE-Heater/', // This must match your repository name exactly
+  base: '/PTFE-Heater/', // <--- ADD THIS LINE
 })
